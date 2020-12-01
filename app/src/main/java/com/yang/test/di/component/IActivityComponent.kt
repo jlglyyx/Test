42570a4c60ffa1af399de47a28c0be9ecf143f80
yyx <1941008232@qq.com>
2020-12-01 10:23:14 +0800

    feat:单独lib包

--- app/src/main/java/com/yang/test/di/component/IActivityComponent.kt
+++ app/src/main/java/com/yang/test/di/component/IActivityComponent.kt
@@ -3,7 +3,6 @@ package com.yang.test.di.component
 import com.yang.common_lib.remote.di.component.RemoteComponent
 import com.yang.common_lib.scope.ActivityScope
 import com.yang.test.MainActivity
-import com.yang.test.activity.AActivity
 import com.yang.test.di.module.MActivityModule
 import dagger.Component
 
